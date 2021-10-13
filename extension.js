0694fb4e3c56d3ebb9b7eb91097f171fba1fb94b
Adam <duonggiakhanhb@gmail.com>
2021-10-13 12:26:47 +0700

    recode

--- extension.js
+++ extension.js
@@ -6,41 +6,53 @@ const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 
-let on = Gio.icon_new_for_string(Me.dir.get_path() + "/on.svg");
-let off = Gio.icon_new_for_string(Me.dir.get_path() + "/off.svg");
-let myPopup;
 
-const MyPopup = GObject.registerClass(
-class MyPopup extends PanelMenu.Button {
+let cloudflare;
+
+
+
+const Cloudflare = GObject.registerClass(
+class Cloudflare extends PanelMenu.Button {
 
   _init () {
-  
+    let on = Gio.icon_new_for_string(Me.dir.get_path() + "/icons/on.svg");
+    let off = Gio.icon_new_for_string(Me.dir.get_path() + "/icons/off.svg");
     super._init(0);
-    var [ok, out, err, exit] = GLib.spawn_command_line_sync('warp-cli status');
+    try{
+      var [ok, out, err, exit] = this.warpCommand('warp-cli status');
+    }
+    catch(e){
+      out = '';
+    }
     let state = out.includes('Connected') ? true : false;
-
     let icon = new St.Icon({
       //icon_name : 'security-low-symbolic',
       gicon : state ? on : off,
       style_class : 'cloud-icons',
     });
-
-    
     this.add_child(icon);
-    log('run');
     this.connect('button-press-event', () => {
-      GLib.spawn_command_line_sync('warp-cli disconnect');
       if(state) {
+        this.warpCommand('warp-cli disconnect')
         state = false;
         icon.gicon = off;
       }
       else {
-        GLib.spawn_command_line_sync('warp-cli connect');
+        [ok, out, err, exit] = this.warpCommand('warp-cli connect');
         state = true;
         icon.gicon = on;
       }
     });
-    
+  }
+  warpCommand(commandLine){
+    try {
+      var [ok, out, err, exit] =  GLib.spawn_command_line_sync(commandLine);
+      return [ok, out, err, exit];
+    }
+    catch (e){
+      this.installedCheck = false;
+      logError(e)
+    }
   }
 
 });
@@ -49,10 +61,11 @@ function init() {
 }
 
 function enable() {
-  myPopup = new MyPopup();
-  Main.panel.addToStatusArea('Cloudflare', myPopup, 1);
+  cloudflare = new Cloudflare();
+  Main.panel.addToStatusArea('Cloudflare', cloudflare, 1, 'right');
 }
 
 function disable() {
-  myPopup.destroy();
+  cloudflare.destroy();
+  cloudflare = null;
 }
